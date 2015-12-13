07f7d53644bb689fe3258169dcc244a30269f576
jens.klose <jens.klose@googlemail.com>
2015-12-13 22:45:26 +0100

    remove example git repo on first startup

--- start.sh
+++ start.sh
@@ -3,6 +3,16 @@
 . ./project.sh
 
 if ! [ -d "$PROJECT_PATH/.git" ]; then
+  # remove the example git-remote link
+  cd "$CONFIG_PATH"
+  git remote -v |grep origin|grep "https://github.com/mazehall/eintopf-php-dev" >/dev/null
+  if [ $? -eq 0 ]
+  then
+    echo "REMOVE GIT REMOTE: remote origin should point to your own repository."
+    git remote remove origin
+  fi
+
+  # install demo PHP sources
   echo "cloning php sources..."
   if ! xgit clone https://github.com/mazelab/php-slim-example "$PROJECT_PATH"; then
     echo "git clone failed"
