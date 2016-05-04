c21de03e019e3b032f8263f3fc95e263e5559fb7
Marcel Kilian <cdskilian@googlemail.com>
2016-05-04 13:47:53 +0200

    added composer --ansi flag in start.sh

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ fi
 
 if ! [ -d "$PROJECT_PATH/web/vendor" ]; then
   echo "installing composer dependencies..."
-  if ! xcompose "run --rm --no-deps web sh -c 'cd /app && composer install'"; then
+  if ! xcompose "run --rm --no-deps web sh -c 'cd /app && composer --ansi install'"; then
     echo "installing composer dependencies failed"
     exit 1
   fi
